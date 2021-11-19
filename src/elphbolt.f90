b33df05b633027ad96d831cb3fb5e53706a007ea
Nakib Haider <nakib.haider.protik@gmail.com>
2021-11-19 16:27:23 +0100

    Timer implemented

--- src/elphbolt.f90
+++ src/elphbolt.f90
@@ -24,7 +24,7 @@ program elphbolt
   !! electron-phonon and phonon-phonon interactions computed ab initio.
 
   use params, only: k8, dp
-  use misc, only: welcome, print_message, subtitle
+  use misc, only: welcome, print_message, subtitle, timer
   use numerics_module, only: numerics
   use crystal_module, only: crystal
   use symmetry_module, only: symmetry
@@ -46,9 +46,14 @@ program elphbolt
   type(electron) :: el
   type(phonon) :: ph
   type(bte) :: bt
+  type(timer) :: t_all, t_event
   
   !Print banner and other information
-  call welcome  
+  call welcome
+
+  call t_all%start_timer('elphbolt')
+
+  call t_event%start_timer('Initialization')
   
   !Set up crystal
   call crys%initialize
@@ -66,12 +71,20 @@ program elphbolt
      !Calculate electrons
      call el%initialize(wann, crys, sym, num)
   end if
+
+  call t_event%end_timer('Initialization')
+  
+  call t_event%start_timer('Phonons')
   
   !Calculate phonons
   call ph%initialize(crys, sym, num)
 
+  call t_event%end_timer('Phonons')
+  
   select case(num%runlevel)
   case(1) !BTE solving case
+     call t_event%start_timer('Density of states and one-particle scattering rates')
+
      call subtitle("Calculating density of states...")
      if(num%onlyebte .or. num%drag) then
         !Calculate electron density of states
@@ -95,11 +108,17 @@ program elphbolt
      call calculate_bound_scatt_rates(ph%prefix, num%phbound, crys%bound_length, &
           ph%vels, ph%indexlist_irred, bt%ph_rta_rates_bound_ibz)
 
+     call t_event%end_timer('Density of states and one-particle scattering rates')
+     
      if(num%plot_along_path) then
+        call t_event%start_timer('Plots along path')
+        
         call subtitle("Plotting along high-symmetry path...")
 
         !Plot electron bands, phonon dispersions, and g along path.
         call wann%plot_along_path(crys, num)
+
+        call t_event%end_timer('Plots along path')
      end if
 
      call subtitle("Calculating interactions...")
@@ -109,28 +128,44 @@ program elphbolt
 
      if(num%onlyphbte .and. num%phe .or. num%drag) then
         if(.not. num%read_gq2) then
+           call t_event%start_timer('IBZ q e-ph interactions')
+           
            !Calculate mixed Bloch-Wannier space e-ph vertex g(Re,q)
            call calculate_gReq(wann, ph, num)
 
            !Calculate Bloch space e-ph vertex g(k,q) for IBZ q
            call calculate_eph_interaction_ibzq(wann, crys, el, ph, num, 'g')
+
+           call t_event%end_timer('IBZ q e-ph interactions')
         end if
 
+        call t_event%start_timer('IBZ ph-e transition probilities')
+        
         !Calculate ph-e transition probabilities
         call calculate_eph_interaction_ibzq(wann, crys, el, ph, num, 'Y')
+        
+        call t_event%end_timer('IBZ ph-e transition probilities')
      end if
 
      if(num%onlyebte .or. num%drag) then
         if(.not. num%read_gk2) then
+           call t_event%start_timer('IBZ k e-ph interactions')
+           
            !Calculate mixed Bloch-Wannier space e-ph vertex g(k,Rp)
            call calculate_gkRp(wann, el, num)
 
            !Calculate Bloch space e-ph vertex g(k,q) for IBZ k
            call calculate_eph_interaction_ibzk(wann, crys, el, ph, num, 'g')
+
+           call t_event%end_timer('IBZ k e-ph interactions')
         end if
+
+        call t_event%start_timer('IBZ e-ph transition probabilities')
 
         !Calculate e-ph transition probabilities
         call calculate_eph_interaction_ibzk(wann, crys, el, ph, num, 'X')
+        
+        call t_event%end_timer('IBZ e-ph transition probabilities')
      end if
 
      if(num%onlyebte .or. num%drag .or. num%phe .or. num%drag &
@@ -146,20 +181,32 @@ program elphbolt
 
      if(num%onlyebte .or. num%drag) then
         if(num%elchimp) then
+           call t_event%start_timer('e-ch. imp. interactions')
+           
            !Calculate e-ch. imp. transition probabilities
            call calculate_echimp_interaction_ibzk(crys, el, num)
+
+           call t_event%end_timer('e-ch. imp. interactions')
         end if
      end if
 
      if(num%onlyphbte .or. num%drag) then
         if(.not. num%read_V) then
+           call t_event%start_timer('IBZ q ph-ph interactions')
+           
            !Calculate ph-ph vertex
            call calculate_3ph_interaction(ph, crys, num, 'V')
+
+           call t_event%end_timer('IBZ q ph-ph interactions')
         end if
 
         if(.not. num%read_W) then
+           call t_event%start_timer('IBZ ph-ph scattering rates')
+           
            !Calculate ph-ph transition probabilities
            call calculate_3ph_interaction(ph, crys, num, 'W')
+           
+           call t_event%end_timer('IBZ ph-ph scattering rates')
         end if
      end if
 
@@ -185,5 +232,7 @@ program elphbolt
      end if
   end select
 
+  call t_all%end_timer('elphbolt')
+  
   call print_message('______________________Thanks for using elphbolt. Bye!______________________')
 end program elphbolt
