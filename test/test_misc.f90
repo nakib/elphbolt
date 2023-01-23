4c99add2fe9fc7c64d8deac00dbe2fb145eef52d
Nakib Haider <nakib.haider.protik@gmail.com>
2023-01-23 15:42:52 +0100

    Wrote a few more unit tests.

--- test/test_misc.f90
+++ test/test_misc.f90
@@ -4,14 +4,14 @@ program test_misc
   use testify_m, only : testify
   use params, only: pi
   use misc, only: int_div, expi, trace, kronecker, sort, cross_product, &
-       twonorm, binsearch
+       twonorm, binsearch, mux_vector, demux_vector, interpolate
   
   implicit none
 
   integer :: itest
-  integer, parameter :: num_tests = 11
+  integer, parameter :: num_tests = 13
   type(testify) :: test_array(num_tests), tests_all
-  integer(i64) :: index, quotient, remainder, int_array(5)
+  integer(i64) :: index, quotient, remainder, int_array(5), v1(3), v2(3), v1_muxed, v2_muxed
   real(r64) :: pauli1(2, 2), ipauli2(2, 2), pauli3(2, 2), &
        real_array(5)
 
@@ -41,7 +41,7 @@ program test_misc
 
   !cross_product
   itest = itest + 1
-  test_array(itest) = testify("cross_product i x j; j x k; i x k")
+  test_array(itest) = testify("cross_product i x j, j x k, i x k")
   call test_array(itest)%assert(&
        [cross_product([1.0_r64, 0.0_r64, 0.0_r64], [0.0_r64, 1.0_r64, 0.0_r64]), &
         cross_product([0.0_r64, 1.0_r64, 0.0_r64], [0.0_r64, 0.0_r64, 1.0_r64]), &
@@ -59,21 +59,21 @@ program test_misc
   
   !expi
   itest = itest + 1
-  test_array(itest) = testify("expi 0; pi")
+  test_array(itest) = testify("expi 0, pi")
   call test_array(itest)%assert(&
        [expi(0.0_r64), expi(pi)], &
        [(1.0_r64, 0.0_r64), cmplx(cos(pi), sin(pi), r64)])
   
   !twonorm_real_rank1, *_rank2
   itest = itest + 1
-  test_array(itest) = testify("twonorm rank-1; rank-2")
+  test_array(itest) = testify("twonorm rank-1, rank-2")
   call test_array(itest)%assert(&
        [twonorm([sqrt(1.0_r64/3.0_r64), -sqrt(1.0_r64/3.0_r64), sqrt(1.0_r64/3.0_r64)]), twonorm(pauli1)], &
        [1.0_r64, sqrt(2.0_r64)])
   
   !trace
   itest = itest + 1
-  test_array(itest) = testify("trace pauli1; ipauli2; pauli3, -ipauli1..3")
+  test_array(itest) = testify("trace pauli1, ipauli2, pauli3, -ipauli1..3")
   call test_array(itest)%assert(&
        [trace(pauli1), trace(ipauli2), trace(pauli3), trace(-matmul(pauli1, matmul(ipauli2, pauli3)))], &
        [0.0_r64, 0.0_r64, 0.0_r64, 2.0_r64])
@@ -109,9 +109,23 @@ program test_misc
   !compsimps
 
   !mux_vector
+  itest = itest + 1
+  test_array(itest) = testify("mux_vector base 0, base 1")
+  call test_array(itest)%assert(&
+       [mux_vector(1_i64*[1, 2, 3], 1_i64*[4, 4, 4], 0_i64), &
+        mux_vector(1_i64*[1, 2, 3], 1_i64*[4, 4, 4], 1_i64)], &
+       [58_i64, 37_i64])
 
   !demux_vector
-
+  itest = itest + 1
+  test_array(itest) = testify("demux_vector base 0, base 1")
+  v1_muxed = 58_i64 !a test muxed vector
+  v2_muxed = 37_i64 !another test muxed vector
+  call demux_vector(v1_muxed, v1, 1_i64*[4, 4, 4], 0_i64)
+  call demux_vector(v2_muxed, v2, 1_i64*[4, 4, 4], 1_i64)
+  call test_array(itest)%assert(&
+       [v1, v2], 1_i64*[1, 2, 3, 1, 2, 3])
+  
   !demux_mesh
 
   !mux_state
