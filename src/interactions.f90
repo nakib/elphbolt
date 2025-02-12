e15a559ec4915cf555249ff4515f463da998d77f
Dwaipayan Paul <dwaipayanpaul6@gmail.com>
2025-02-12 16:29:41 +0100

    Implemented RPA screening in elch interaction

--- src/interactions.f90
+++ src/interactions.f90
@@ -64,8 +64,9 @@ module interactions
   
 contains
   
-  pure real(r64) function gchimp2(el, crys, qcrys, evec_k, evec_kp)
-    !! Function to calculate the squared electron-charged impurity vertex.
+  pure real(r64) function gchimp2_TF(el, crys, qcrys, evec_k, evec_kp)
+    !! Function to calculate the Thomas Fermi screened squared 
+    !! electron-charged impurity vertex
     !!
     !! The expression implemented here was derived by Leveillee et al.
     !! in PRB 107, 125207 (2023) with the G-dependent dielectric
@@ -77,7 +78,7 @@ contains
     complex(r64),intent(in) :: evec_k(:), evec_kp(:)
 
     real(r64) :: qcart(3), prefac, overlap, Gsum, &
-         Gplusq(3), eps_3x3(3, 3)
+         Gplusq(3), eps_3x3(3, 3), screened_qTF_sq
     integer :: ik1, ik2, ik3
         
     qcart = matmul(crys%reclattvecs, qcrys)
@@ -99,9 +100,10 @@ contains
                   + ik2*crys%reclattvecs(:, 2) &
                   + ik3*crys%reclattvecs(:, 3)  ) + qcart
 
+             screened_qTF_sq = crys%qTF**2/crys%epsiloninf
              !Following Eq. 7 of Nat. Comm. 12:2222 (2021)
              !G + q dependent dielectric function
-             eps_3x3 = (crys%epsilon0 + (crys%qTF/twonorm(Gplusq))**2)*eye(3_i64)
+             eps_3x3 = (crys%epsiloninf + (crys%qTF/twonorm(Gplusq))**2)*eye(3_i64)
 
              !Only want G /= -q in the sum over G
              if(all(Gplusq /= 0)) Gsum = Gsum + &
@@ -110,9 +112,59 @@ contains
        end do
     end do
 
-    gchimp2 = prefac*overlap*Gsum !ev^2
-  end function gchimp2
+    gchimp2_TF = prefac*overlap*Gsum !ev^2
+  end function gchimp2_TF
 
+  pure real(r64) function gchimp2_RPA(el, crys, qcrys, evec_k, evec_kp, X0_qw)
+    !! Function to calculate the squared electron-charged impurity vertex.
+    !!
+    !! The expression implemented here was derived by Leveillee et al.
+    !! in PRB 107, 125207 (2023) with the G-dependent dielectric
+    !! from Ganose et al. Nat. Comm. 12:2222 (2021).
+
+    type(crystal), intent(in) :: crys
+    type(electron), intent(in) :: el
+    real(r64), intent(in) :: qcrys(3)
+    complex(r64), intent(in) :: X0_qw
+    complex(r64),intent(in) :: evec_k(:), evec_kp(:)
+
+    real(r64) :: qcart(3), prefac, overlap, Gsum, &
+         Gplusq(3)
+    complex(r64) :: eps_3x3(3, 3)
+    integer :: ik1, ik2, ik3
+        
+    qcart = matmul(crys%reclattvecs, qcrys)
+    
+    !This is [U(k')U^\dagger(k)]_nm squared
+    !(Recall that the electron eigenvectors came out daggered from el_wann_epw.)
+    overlap = (abs(dot_product(evec_kp, evec_k)))**2
+
+    prefac = 1.0e-3_r64/crys%volume/perm0**2*&
+         (el%chimp_conc_n*(qe*el%Zn)**2 + el%chimp_conc_p*(qe*el%Zp)**2)
+
+    Gsum = 0.0_r64
+    !Use a safe range for the G vector sums
+    !This is likely a severe overkill...
+    do ik1 = -3, 3
+       do ik2 = -3, 3
+          do ik3 = -3, 3
+             Gplusq = (  ik1*crys%reclattvecs(:, 1) &
+                  + ik2*crys%reclattvecs(:, 2) &
+                  + ik3*crys%reclattvecs(:, 3)  ) + qcart
+
+             !G + q dependent dielectric function
+             eps_3x3 = (crys%epsiloninf - prefac*X0_qw/twonorm(Gplusq)**2)*eye(3_i64)
+
+             !Only want G /= -q in the sum over G
+             if(all(Gplusq /= 0)) Gsum = Gsum + &
+                  abs(1.0_r64/dot_product(Gplusq, matmul(eps_3x3, Gplusq)))**2
+          end do
+       end do
+    end do
+
+    gchimp2_RPA = prefac*overlap*Gsum !ev^2
+  end function gchimp2_RPA
+  
   pure real(r64) function gCoul2_TF(el, crys, qcrys, evec_k, evec_kp)
     !! Function to calculate the Thomas-Fermi screened
     !! squared electron-electron vertex.
@@ -2531,12 +2583,15 @@ contains
     
     !Local variables
     integer(i64) :: nstates_irred, istate, m, ik, n, ikp, &
-         start, end, chunk, k_indvec(3), kp_indvec(3), &
-         q_indvec(3), count, nprocs, num_active_images
-    real(r64) :: k(3), kp(3), q_crys(3), const, en_el, delta, g2
+         start, end, chunk, count, nprocs, num_active_images
+    real(r64) :: k(3), kp(3), q_crys(3), const, en_el, en_el_p, delta, g2
     real(r64), allocatable :: Xchimp_istate(:)
     integer(i64), allocatable :: istate_el(:)
     character(len = 1024) :: filename
+    real(r64), allocatable :: Omegas_cont(:), specX0_cont(:), ImX0_cont(:), &
+         ReX0_cont(:)
+    complex(r64) :: temp(1), X0_qw 
+    type(vec) :: kp_vec, k_vec, q_vec
     procedure(delta_fn), pointer :: delta_fn_ptr => null()
 
     call print_message("Calculating e-ch. imp. transition probabilities for all IBZ electrons...")
@@ -2555,6 +2610,14 @@ contains
     
     !Total number of IBZ blocks states
     nstates_irred = el%nwv_irred*el%numbands
+    
+    if(num%elel_screening_type == 'RPA') then
+       !Allocate and create continuous energy mesh over around the Fermi shell
+       allocate(Omegas_cont(num%ncont_mesh), specX0_cont(num%ncont_mesh), &
+            ImX0_cont(num%ncont_mesh), ReX0_cont(num%ncont_mesh))
+
+       call linspace(Omegas_cont, -2*el%fsthick, 2*el%fsthick, num%ncont_mesh) 
+    end if
     
     call distribute_points(nstates_irred, chunk, start, end, num_active_images)
     
@@ -2576,10 +2639,8 @@ contains
           if(abs(en_el - el%enref) > el%fsthick) cycle
 
           !Initial (IBZ blocks) wave vector (crystal coords.)
-          k = el%wavevecs_irred(ik, :)
-
-          !Convert from crystal to 0-based index vector
-          k_indvec = nint(k*el%wvmesh)
+          !k = el%wavevecs_irred(ik, :)
+          k_vec = vec(el%indexlist_irred(ik), el%wvmesh, crys%reclattvecs)
 
           !Initialize eligible process counter for this state
           count = 0
@@ -2587,29 +2648,44 @@ contains
           !Run over final (FBZ blocks) electron wave vectors
           do ikp = 1, el%nwv
              !Final wave vector (crystal coords.)
-             kp = el%wavevecs(ikp, :)
+             !kp = el%wavevecs(ikp, :)
+             kp_vec = vec(el%indexlist(ikp), el%wvmesh, crys%reclattvecs)
+             !q \equiv kp - k
+             q_vec = vec_sub(kp_vec, k_vec, el%wvmesh, crys%reclattvecs) 
 
-             !Convert from crystal to 0-based index vector
-             kp_indvec = nint(kp*el%wvmesh)
+             if(num%elel_screening_type == 'RPA') then
+                !Calculate polarizablity
+                call spectral_head_polarizability_3d_q(&
+                     ImX0_cont, Omegas_cont, q_vec, el, crys, num%tetrahedra)
+                ImX0_cont = -pi*ImX0_cont
 
-             !Find interacting phonon wave vector
-             !Note that q, k, and k' are all on the same mesh
-             q_indvec = kp_indvec - k_indvec !0-based index vector
-
-             !Above, but in crystal coordinates
-             q_crys = q_indvec/dble(el%wvmesh)
-                          
+                call hilbert_transform(-ImX0_cont, ReX0_cont)
+             end if
              !Run over final electron bands
              do n = 1, el%numbands
+                ! Energy of final electron
+                en_el_p = el%ens(ikp, n)
                 !Apply energy window to final electron
-                if(abs(el%ens(ikp, n) - el%enref) > el%fsthick) cycle
+                if(abs(en_el_p - el%enref) > el%fsthick) cycle
                 
                 !Increment g2 processes counter
                 count = count + 1
 
-                !Calculate matrix element
-                g2 = gchimp2(el, crys, q_crys, &
+                ! Squared matrix element screened by Thomas-Fermi or RPA dielectric.
+                ! q = 0 divergence case is handled by the Thomas-Fermi screening.
+                if(all(q_crys == 0) .or. num%elel_screening_type == 'TF') then
+                   !Calculate matrix element
+                   g2 = gchimp2_TF(el, crys, q_crys, &
                      el%evecs_irred(ik, m, :), el%evecs(ikp, n, :))
+                else
+                   !Interpolating polarizability from continuous mesh to sampling energy
+                   temp = interpolator_1d([(en_el_p - en_el)], Omegas_cont, ReX0_cont) &
+                        + oneI*interpolator_1d([(en_el_p - en_el)], Omegas_cont, ImX0_cont)
+                   X0_qw = temp(1)
+
+                   g2 = gchimp2_RPA(el, crys, q_crys, &
+                     el%evecs_irred(ik, m, :), el%evecs(ikp, n, :), X0_qw)
+                end if
 
                 !Evaulate delta function
                 delta = delta_fn_ptr(en_el, ikp, n, el%wvmesh, el%simplex_map, &
