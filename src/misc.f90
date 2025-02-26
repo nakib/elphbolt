4b5b0d892d77982a08be771de044af6441aa8cb9
Nakib H. Protik <nakib.haider.protik@gmail.com>
2025-02-26 10:35:50 +0100

    Added a template for permutations

--- src/misc.f90
+++ src/misc.f90
@@ -129,6 +129,15 @@ contains
        eye(i, i) = 1
     end do
   end function eye
+
+  pure function permtutations(N)
+    !! Returns all permutations of an array of size N
+
+    integer(i64), intent(in) :: N
+    integer(i64) :: permutations(N)
+
+    !TODO
+  end function permtutations
   
   subroutine linspace(grid, min, max, num)
     !! Create equidistant grid.
