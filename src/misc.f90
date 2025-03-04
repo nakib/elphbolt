44b465977bc564f4a87a237ecedf157956246e4c
Sally issa <sally.issa1@outlook.com>
2025-03-04 11:18:02 +0100

    Added permutation generator

--- src/misc.f90
+++ src/misc.f90
@@ -130,91 +130,78 @@ contains
     end do
   end function eye
   
- !factorial(N)
- pure function factorial(N) result(fact)
-        implicit none
-        integer(i64), intent(in) :: N
-        integer(i64) :: fact, i
-        
-        ! Handle edge case for factorial(0) = 1
-        if (n == 0) then
-            fact = 1
-            return
-        end if
+  ! Permutations function
+  pure function permutations(N) result(perms)
+    !! Returns the number of permutations of N
+     
+    integer(i64), intent(in) :: N
+    integer(i64) :: fact, perm_count
+    integer(i64), allocatable :: perms(:,:)
+    integer(i64) :: p(N), dir(N)
+    integer(i64) :: i, t, q, s, temp
 
-        fact = 1
-        do i = 2, N
-            fact = fact * i
-        end do
-    end function factorial
-       
- ! Permutations function
-    pure function permutations(N) result(perms)
-        implicit none
-        integer(i64), intent(in) :: N
-        integer(i64) :: fact, perm_count
-        integer(i64), allocatable :: perms(:,:)
-        integer(i64) :: p(N), dir(N)
-        integer(i64) :: i, t, q, s, temp
-        fact = factorial(N)
-        allocate(perms(fact, N))
+    ! Use gamma function to compute factorial(N) as gamma(N)=(N-1)!
+    fact = int(gamma(real(N+1, kind=8)))
 
-! Initialize permutation array
+    allocate(perms(fact, N))
+
+    ! Initialize permutation array
     do i = 1, N
-        p(i) = i
-        dir(i) = -1  ! -1 means left, 1 means right
+       p(i) = i
+       dir(i) = -1  ! -1 means left, 1 means right
     end do
 
     perm_count = 1
     perms(perm_count, :) = p
-    
+
     do
-        q = 0
-        t = 0
+       q = 0
+       t = 0
 
- !loop to find largest mobile element
-        do i = 1, N
-            if (dir(i) == -1 .and. i > 1) then
-                if (p(i) > p(i-1) .and. p(i) > q) then
-                    q = p(i)
-                    t = i
-                end if
-            else if (dir(i) == 1 .and. i < N) then
-                if (p(i) > p(i+1) .and. p(i) > q) then
-                    q = p(i)
-                    t = i
-                end if
-            end if
-        end do
+       ! Loop to find the largest mobile element
+       do i = 1, N
+          if (dir(i) == -1 .and. i > 1) then
+             if (p(i) > p(i-1) .and. p(i) > q) then
+                q = p(i)
+                t = i
+             end if
+          else if (dir(i) == 1 .and. i < N) then
+             if (p(i) > p(i+1) .and. p(i) > q) then
+                q = p(i)
+                t = i
+             end if
+          end if
+       end do
 
-        ! If no mobile element found, exit
-        if (q == 0) exit
+       ! If no mobile element found, exit
+       if (q == 0) exit
 
-        ! Compute new position safely
-        s = t + dir(t)
+       ! Compute new position safely
+       s = t + dir(t)
 
-        ! Ensure s stays within valid bounds
-        if (s < 1 .or. s > N) exit
+       ! Ensure s stays within valid bounds
+       if (s < 1 .or. s > N) exit
 
-        ! Swap p(t) and p(s)
-        temp = p(t)
-        p(t) = p(s)
-        p(s) = temp
+       ! Swap p(t) and p(s)
+       temp = p(t)
+       p(t) = p(s)
+       p(s) = temp
 
-        ! Swap directions
-        temp = dir(t)
-        dir(t) = dir(s)
-        dir(s) = temp
+       ! Swap directions
+       temp = dir(t)
+       dir(t) = dir(s)
+       dir(s) = temp
 
-        perm_count = perm_count + 1
-        perms(perm_count, :) = p
+       perm_count = perm_count + 1
+       perms(perm_count, :) = p
 
-        ! Reverse direction of all elements larger than q
-        do i = 1, N
-            if (p(i) > q) dir(i) = -dir(i)
-        end do
+       ! Reverse direction of all elements larger than q
+       do i = 1, N
+          if (p(i) > q) dir(i) = -dir(i)
+       end do
     end do
-end function permutations 
+  end function permutations
+
     
   subroutine linspace(grid, min, max, num)
     !! Create equidistant grid.
--- test/test_misc.f90
+++ test/test_misc.f90
@@ -8,7 +8,7 @@ program test_misc
        unique, linspace, compsimps, mux_state, demux_state, demux_mesh, expm1, &
        Fermi, Bose, Pade_continued, precompute_interpolation_corners_and_weights, &
        interpolate_using_precomputed, operator(.umklapp.), shrink, Hilbert_transform, &
-       interpolator_1d, permutations, factorial
+       interpolator_1d, permutations
   
   implicit none
 
@@ -51,31 +51,29 @@ program test_misc
   call int_div(3_i64, 10_i64, quotient, remainder)
   call test_array(itest)%assert([quotient, remainder], [0_i64, 3_i64])
    
+  ! Test permutations(N)
+  itest = itest + 1
+  test_array(itest) = testify("permutations(N)")
+  N = 3
+  fact = int(gamma(real(N+1, kind=8)))  ! Use gamma(N+1) instead of factorial
+  perm = permutations(N)  
+  print *, "Expected factorial:", fact
+  print *, "Actual array size:", size(perm, 1)
+  print *, "Generated permutations(", N, "):"
+  do i = 1, size(perm, 1)
+     print *, perm(i, :)
+  end do
+  actual_size_1 = size(perm, 1)
+  actual_size_2 = size(perm, 2)
+  ! Assert that the number of rows matches the expected factorial value
+  call test_array(itest)%assert(actual_size_1, fact)  
+  call test_array(itest)%assert(actual_size_2, N)  
+  ! Ensure each row contains all numbers from {1,..., N}
+  do i = 1, size(perm, 1) 
+     call test_array(itest)%assert([sum(perm(i, :))], [sum([(j, j=1,N)])])
+  end do
+  deallocate(perm)
 
-! Test permutations(N)
-itest = itest + 1
-test_array(itest) = testify("permutations(N)")
-N = 3
-fact = factorial(N)  
-perm = permutations(N)  
-print *, "Expected factorial:", fact
-print *, "Actual array size:", size(perm, 1)
-print *, "Generated permutations(", N, "):"
-do i = 1, size(perm, 1)  
-    print *, perm(i, :)
-end do
-actual_size_1 = size(perm, 1)
-actual_size_2 = size(perm, 2)
-call test_array(itest)%assert(actual_size_1, fact)  
-call test_array(itest)%assert(actual_size_2, N)  
-! Ensure each row contains all numbers from {1,...,N}
-do i = 1, size(perm, 1)  ! Use size(perm,1) to avoid out-of-bounds error
-    call test_array(itest)%assert([sum(perm(i, :))], [sum([(j, j=1,N)])])
-end do
-deallocate(perm)
-
-
-  
   !distribute_points
   !TODO This is a coarray dependent test. Will revisit.
 
