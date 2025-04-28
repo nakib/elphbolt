8009bc45564171adc89837289706165d1434b18f
Dwaipayan Paul <dwaipayanpaul6@gmail.com>
2025-04-28 09:47:38 +0200

    Reverted original Hilbert test

--- test/test_misc.f90
+++ test/test_misc.f90
@@ -381,28 +381,28 @@ program test_misc
   allocate(hfx1_even(n_even))
   call Hilbert_transform(fx1(x_even), hfx1_even)
   call test_array(itest)%assert(hfx1_even(ind_even), hfx1(x_even(ind_even)), &
-       tol = 2e-3_r64)
+       tol = 2e-4_r64)
 
   itest = itest + 1
   test_array(itest) = testify("Hilbert transform: f(x) = sin(x)/(1 + x^2), even points")
   allocate(hfx2_even(n_even))
   call Hilbert_transform(fx2(x_even), hfx2_even)
   call test_array(itest)%assert(hfx2_even(ind_even), hfx2(x_even(ind_even)), &
-       tol = 1e-3_r64)
+       tol = 1e-4_r64)
 
   itest = itest + 1
   test_array(itest) = testify("Hilbert transform: f(x) = 1/(1 + x^2), odd points")
   allocate(hfx1_odd(n_odd))
   call Hilbert_transform(fx1(x_odd), hfx1_odd)
   call test_array(itest)%assert(hfx1_odd(ind_odd), hfx1(x_odd(ind_odd)), &
-       tol = 4e-3_r64)
+       tol = 4e-4_r64)
 
   itest = itest + 1
   test_array(itest) = testify("Hilbert transform: f(x) = sin(x)/(1 + x^2), odd points")
   allocate(hfx2_odd(n_odd))
   call Hilbert_transform(fx2(x_odd), hfx2_odd)
   call test_array(itest)%assert(hfx2_odd(ind_odd), hfx2(x_odd(ind_odd)), &
-       tol = 1e-3_r64)
+       tol = 1e-5_r64)
   
   ! 1D Interpolation
   itest = itest + 1
